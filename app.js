302ba9b514601cd2f5ae5fa56de36ae2480c1b3f
lnosal1 <lnosal1@live.maryville.edu>
2022-03-27 14:04:13 -0500

    modal updates

--- app.js
+++ app.js
@@ -21,28 +21,3 @@ var quizAPI = (Q1Size,Q2Climate,Q3Roll,Q4Arms)=>{
     .catch(error => console.log('error', error));
 }
 
-// Get the modal
-var modal = document.getElementById("myModal");
-
-// Get the button that opens the modal
-var btn = document.getElementById("emailmodal");
-
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
-
-// When the user clicks on the button, open the modal
-var launchmodal = function() {
-  modal.style.display = "block";
-}
-
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-  modal.style.display = "none";
-}
-
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
-  }
-} 
