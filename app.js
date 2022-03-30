158de3c0f4106b340617c4b4bd394a08b4ca80f4
lnosal1 <lnosal1@live.maryville.edu>
2022-03-29 20:52:00 -0500

    user API

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 // define the callAPI function that takes quiz answers as parameters
-var quizAPI = (Q1Size,Q2Climate,Q3Roll,Q4Arms)=>{
+var quizAPI = (email,Q1Size,Q2Climate,Q3Roll,Q4Arms)=>{
     // instantiate a headers object
     var myHeaders = new Headers();
     // add content type header to object
     myHeaders.append("Content-Type", "application/json");
     // using built in JSON utility package turn object to string and store in a variable
-    var answers = JSON.stringify({"Q1Size":Q1Size,"Q2Climate":Q2Climate,"Q3Roll":Q3Roll,"Q4Arms":Q4Arms});
+    var answers = JSON.stringify({"email":email,"Q1Size":Q1Size,"Q2Climate":Q2Climate,"Q3Roll":Q3Roll,"Q4Arms":Q4Arms});
 
     // create a JSON object with parameters for API call and store in a variable
     var requestOptions = {
