bec1f706b1132ae8b4f822c99db51c21677e95a5
lnosal1 <lnosal1@live.maryville.edu>
2022-03-29 20:31:42 -0500

    user API update

--- app.js
+++ app.js
@@ -21,3 +21,25 @@ var quizAPI = (Q1Size,Q2Climate,Q3Roll,Q4Arms)=>{
     .catch(error => console.log('error', error));
 }
 
+// define the callAPI function that takes quiz answers as parameters
+var userAPI = (fName,lName,email)=>{
+    // instantiate a headers object
+    var myuserHeaders = new Headers();
+    // add content type header to object
+    myuserHeaders.append("Content-Type", "application/json");
+    // using built in JSON utility package turn object to string and store in a variable
+    var userinfo = JSON.stringify({"FirstName":fName,"LastName":lName, "Email":email});
+
+    // create a JSON object with parameters for API call and store in a variable
+    var requestOptions = {
+        method: 'POST',
+        headers: myuserHeaders,
+        body: userinfo,
+        redirect: 'follow'
+    };
+    // make API call with parameters and use promises to get response
+    fetch("https://nsc5shfxpi.execute-api.us-east-1.amazonaws.com/prod", requestOptions)
+    .then(response => response.text())
+    //.then(result => alert(JSON.parse(result).body))
+    .catch(error => console.log('error', error));
+}
