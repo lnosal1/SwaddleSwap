e8e971056e7c51f96c3dcfc4271b1273b0055845
lnosal1 <lnosal1@live.maryville.edu>
2022-04-05 20:58:44 -0500

    recommendations update

--- data-reccommendations.js
+++ data-reccommendations.js
@@ -11,48 +11,21 @@ $(document).ready(function(){
 	
 	// Define our data objects
 
-    var sleepingBlog = Mustache.render(sleepBlog, {
-        item:
-		[
-			{
-                name: "Sleep Props: How to Avoid Them",
-                description: "Learn more about the sleep props that can impact your child's sleep habits.",
-                button: "Learn More"
-
-            },
+    var sleepingBlog = Mustache.render(sleepBlog, 
+        {"Items": [
             {
-                name: "Should You Use Melatonin?",
-                description: "Learn about why melatonin supplements can actually hurt your child's sleep.",
-                button: "Learn More"
-            },
-            {
-                name: "When is it Time to Night Wean?",
-                description: "How young is too young for night weaning? And how to get started if baby is ready.",
-                button:"Learn More"
-
-            },
-            {
-                name: "Transitioning from 3 to 2 Naps",
-                description: "Nap transitions can come with a regression, learn how to prevent them.",
-                button:"Learn More"
-
-            },
-            {
-                name: "Safe Swaddles for a Rolling Baby",
-                description: "Once baby starts rolling, your options become more limited.",
-                button:"Learn More"
-
-            },
-            {
-                name: "Night Wakings: Hunger or Habit?",
-                description: "Wondering why baby is waking? Read about how you can assess why.",
-                button:"Learn More"
-
+                "displayName": "SwaddleMe Pod", 
+                "buttonText": "Buy on Amazon", 
+                "arms": "down", 
+                "rollboth": "no", 
+                "buyLink": "https://www.amazon.com/SwaddleMe-Pod-Medium-Little-Months/dp/B082XTSM8C/ref=sr_1_2_sspa?crid=2KTDLMYOKPXJE&keywords=swaddle+pod&qid=1648777115&sprefix=swaddle+%2Caps%2C632&sr=8-2-spons&psc=1&spLa=ZW5jcnlwdGVkUXVhbGlmaWVyPUExSE0yV09VMFlRVVlPJmVuY3J5cHRlZElkPUEwODkxMjgzMjdMTDlXWFNLN1dIUSZlbmNyeXB0ZWRBZElkPUEwNzc1ODY2MVk2TzFRUUpWTkhSRiZ3aWRnZXROYW1lPXNwX2F0ZiZhY3Rpb249Y2xpY2tSZWRpcmVjdCZkb05vdExvZ0NsaWNrPXRydWU=", 
+                "imageLink": "https://swaddleswap.s3.amazonaws.com/swaddlepod.jpg", 
+                "ID": "0005", 
+                "sizeRange": "Under 3M", 
+                "material": "cotton"
             }
-            
-
-		]
-    });
+            ]}
+    );
 	
 	
 	
