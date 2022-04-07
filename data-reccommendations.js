616360e3caedad417c4aa51718410a04c844aa1b
lnosal1 <lnosal1@live.maryville.edu>
2022-04-06 20:31:57 -0500

    Update data-reccommendations.js

--- data-reccommendations.js
+++ data-reccommendations.js
@@ -3,7 +3,25 @@ $(document).ready(function(){
 	// Define variables that reference our script tags within the body of our page
     var sleepBlog = $("#sleepBlog").html();
     //call swaddleAPI
-    var API = $.getScript("app.js", swaddleAPI()); 
+    var swaddleAPI = ()=>{
+        // instantiate a headers object
+        var myswaddleHeaders = new Headers();
+        // add content type header to object
+        myswaddleHeaders.append("Content-Type", "application/json");
+        // using built in JSON utility package turn object to string and store in a variable
+    
+        // create a JSON object with parameters for API call and store in a variable
+        var requestOptions = {
+            method: 'GET',
+            headers: myswaddleHeaders,
+            redirect: 'follow'
+        };
+        // make API call with parameters and use promises to get response
+        fetch(" https://4nqem5uyhg.execute-api.us-east-1.amazonaws.com/prod/getswaddlebyquiz?material=cotton&rollboth=no&sizeRange=Under 3M&arms=down", requestOptions)
+        .then(response => response.text())
+        .then(result => JSON.parse(result))
+        .catch(error => console.log('error', error));
+    }; 
     
 	// Have MustacheJS render our script tags
     
@@ -12,7 +30,7 @@ $(document).ready(function(){
 	
 	// Define our data objects
 
-    var sleepingBlog = Mustache.render(sleepBlog, API,
+    var sleepingBlog = Mustache.render(sleepBlog, swaddleAPI(),
         {"Items": [{"displayName": "SwaddleMe Pod", "buttonText": "Buy on Amazon", "arms": "down", "rollboth": "no", "buyLink": "https://www.amazon.com/SwaddleMe-Pod-Medium-Little-Months/dp/B082XTSM8C/ref=sr_1_2_sspa?crid=2KTDLMYOKPXJE&keywords=swaddle+pod&qid=1648777115&sprefix=swaddle+%2Caps%2C632&sr=8-2-spons&psc=1&spLa=ZW5jcnlwdGVkUXVhbGlmaWVyPUExSE0yV09VMFlRVVlPJmVuY3J5cHRlZElkPUEwODkxMjgzMjdMTDlXWFNLN1dIUSZlbmNyeXB0ZWRBZElkPUEwNzc1ODY2MVk2TzFRUUpWTkhSRiZ3aWRnZXROYW1lPXNwX2F0ZiZhY3Rpb249Y2xpY2tSZWRpcmVjdCZkb05vdExvZ0NsaWNrPXRydWU=", "imageLink": "https://swaddleswap.s3.amazonaws.com/swaddlepod.jpg", "ID": "0005", "sizeRange": "Under 3M", "material": "cotton"}], "Count": 1, "ScannedCount": 21, "ResponseMetadata": {"RequestId": "K7LAK7VBET0E8MPKO01GL409M3VV4KQNSO5AEMVJF66Q9ASUAAJG", "HTTPStatusCode": 200, "HTTPHeaders": {"server": "Server", "date": "Wed, 06 Apr 2022 01:46:42 GMT", "content-type": "application/x-amz-json-1.0", "content-length": "730", "connection": "keep-alive", "x-amzn-requestid": "K7LAK7VBET0E8MPKO01GL409M3VV4KQNSO5AEMVJF66Q9ASUAAJG", "x-amz-crc32": "3976968648"}, "RetryAttempts": 0}}
     );
 	
