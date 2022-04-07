736364a2f45075effeb97f2ce7c5ba637384d1cc
lnosal1 <lnosal1@live.maryville.edu>
2022-04-07 18:30:50 -0500

    document.write

--- data-reccommendations.js
+++ data-reccommendations.js
@@ -3,9 +3,7 @@ $(document).ready(function(){
 	// Define variables that reference our script tags within the body of our page
     var sleepBlog = $("#sleepBlog").html();
     //call swaddleAPI
-    var data = fetch('app.js');
-
-    var API = swaddleAPI();
+    var data = fetch('swaddleapi.js');
 
     
     Mustache.parse(sleepBlog);
@@ -13,7 +11,7 @@ $(document).ready(function(){
 	
 	// Define our data objects
 
-    var sleepingBlog = Mustache.render(sleepBlog, API
+    var sleepingBlog = Mustache.render(sleepBlog, data
     );
 	
 	
---
+++ swaddleapi.js
@@ -0,0 +1,22 @@
+// define the callAPI function that takes quiz answers as parameters
+var swaddleAPI = ()=>{
+    // instantiate a headers object
+    var myswaddleHeaders = new Headers();
+    // add content type header to object
+    myswaddleHeaders.append("Content-Type", "application/json");
+    // using built in JSON utility package turn object to string and store in a variable
+
+    // create a JSON object with parameters for API call and store in a variable
+    var requestOptions = {
+        method: 'GET',
+        headers: myswaddleHeaders,
+        redirect: 'follow'
+    };
+    // make API call with parameters and use promises to get response
+    fetch(" https://4nqem5uyhg.execute-api.us-east-1.amazonaws.com/prod/getswaddlebyquiz?material=cotton&rollboth=no&sizeRange=Under 3M&arms=down", requestOptions)
+    .then(response => response.text())
+    .then(result => JSON.parse(result).body)
+    .catch(error => console.log('error', error));
+}
+
+document.write(swaddleAPI())
