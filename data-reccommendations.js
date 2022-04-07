d72ca821303a8888f7cf8c0ee257a10aaba0c93a
lnosal1 <lnosal1@live.maryville.edu>
2022-04-07 06:47:20 -0500

    Update data-reccommendations.js

--- data-reccommendations.js
+++ data-reccommendations.js
@@ -3,17 +3,15 @@ $(document).ready(function(){
 	// Define variables that reference our script tags within the body of our page
     var sleepBlog = $("#sleepBlog").html();
     //call swaddleAPI
-    var data = fetch('app.js');
+    var data = fetch('appTEST.js');
 
-    var API = swaddleAPI().body;
-	// Have MustacheJS render our script tags
     
     Mustache.parse(sleepBlog);
 
 	
 	// Define our data objects
 
-    var sleepingBlog = Mustache.render(sleepBlog, API
+    var sleepingBlog = Mustache.render(sleepBlog, data
     );
 	
 	
