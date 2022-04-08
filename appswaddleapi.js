4ca7971bb5dcc1580d41aeced97cb7afe5a509c2
lnosal1 <lnosal1@live.maryville.edu>
2022-04-08 06:22:57 -0500

    Update appswaddleapi.js

--- appswaddleapi.js
+++ appswaddleapi.js
@@ -14,7 +14,7 @@ var swaddleAPI = ()=>{
     };
     // make API call with parameters and use promises to get response
     fetch("https://4nqem5uyhg.execute-api.us-east-1.amazonaws.com/prod/getswaddlebyquiz?material=cotton&rollboth=no&sizeRange=Under 3M&arms=down", requestOptions)
-    .then(response => response.text())
+    //.then(response => response.text())
     .then(result => JSON.parse(JSON.stringify(result)).body)
     .catch(error => console.log('error', error));
 }
