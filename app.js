e04c79f7c8b31be28890e633e04739d3914282a5
lnosal1 <lnosal1@live.maryville.edu>
2022-04-07 20:26:14 -0500

    alert

--- app.js
+++ app.js
@@ -61,6 +61,6 @@ var swaddleAPI = ()=>{
     // make API call with parameters and use promises to get response
     fetch(" https://4nqem5uyhg.execute-api.us-east-1.amazonaws.com/prod/getswaddlebyquiz?material=cotton&rollboth=no&sizeRange=Under 3M&arms=down", requestOptions)
     .then(response => response.text())
-    .then(result => JSON.parse(result).body)
+    .then(result => alert(JSON.parse(result).body))
     .catch(error => console.log('error', error));
 }
