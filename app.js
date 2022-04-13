e5f1d8955961a903c197e6e18269d9f6521e8343
lnosal1 <lnosal1@live.maryville.edu>
2022-04-12 20:44:33 -0500

    API updates

--- app.js
+++ app.js
@@ -21,3 +21,27 @@ var quizAPI = (fName,lName,email,Q1Size,Q2Climate,Q3Roll,Q4Arms)=>{
     
     .catch(error => console.log('error', error));
 }
+
+// define the callAPI function that takes recommended swaddles as parameters
+var recommendedAPI = (Id, quizanswers, childname, Name, buttonLink, Name2, buttonLink2, Name3, buttonLink3 )=>{
+    // instantiate a headers object
+    var recommendedHeaders = new Headers();
+    // add content type header to object
+    myHeaders.append("Content-Type", "application/json");
+    // using built in JSON utility package turn object to string and store in a variable
+    var swaddles = JSON.stringify({'Id': Id,'quizanswer':quizanswers,'childname': childname,'Name': Name,'buttonLink': buttonLink,'Name2': Name2, 'buttonLink2': buttonLink2,'Name3': Name3,'buttonLink3': buttonLink3,});
+
+    // create a JSON object with parameters for API call and store in a variable
+    var requestOptions = {
+        method: 'POST',
+        headers: recommendedHeaders,
+        body: swaddles,
+        redirect: 'follow',
+        
+    };
+    // make API call with parameters and use promises to get response
+    fetch("https://4nqem5uyhg.execute-api.us-east-1.amazonaws.com/prod", requestOptions)
+    .then(response => response.text())
+    
+    .catch(error => console.log('error', error));
+}
