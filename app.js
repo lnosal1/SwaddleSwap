c84c33862bf4bc2c1f070b0c9ea92391f56ff858
lnosal1 <lnosal1@live.maryville.edu>
2022-04-25 09:20:47 -0500

    update

--- app.js
+++ app.js
@@ -45,3 +45,64 @@ var recommendedAPI = (email, quizanswers, childname, Name, buttonLink, Name2, bu
     
     .catch(error => console.log('error', error));
 }
+
+var engineAPI = (email, quizanswers, childname)=>{
+    fetch('swaddles.json')
+    .then(function (response) {
+        return response.json();
+    })
+    .then(function (data) {
+        appendData(data);
+    })
+    .catch(function (err) {
+        console.log('error: ' + err);
+    });
+function appendData(data) {
+    var mainContainer = document.getElementById("myData1");
+        var div1 = document.createElement("h2");
+        var img = document.createElement("div");
+        var button = document.createElement("div");
+        var Name = data[quizanswers][0].displayName;
+        var imageLink = data[quizanswers][0].imageLink;
+        var buttonLink = data[quizanswers][0].buyLink;
+        div1.innerHTML = Name; 
+        img.innerHTML = "<img src='" + imageLink+ "' class='swaddle' >";
+        button.innerHTML = "<a href='" +buttonLink + "><button type='button' class='content-button'>Buy on Amazon</button></a>";
+        mainContainer.appendChild(img);          
+        mainContainer.appendChild(div1);
+        mainContainer.appendChild(button);
+
+        var mainContainer2 = document.getElementById("myData2");
+        var div2 = document.createElement("h2");
+        var img2 = document.createElement("div");
+        var button2 = document.createElement("div");
+        var Name2 = data[quizanswers][1].displayName;
+        var imageLink2 = data[quizanswers][1].imageLink;
+        var buttonLink2 = data[quizanswers][1].buyLink;
+        div2.innerHTML = Name2; 
+        img2.innerHTML = "<img src='" + imageLink2+ "' class='swaddle' >";
+        button2.innerHTML = "<a href='" +buttonLink2 + "><button type='button' class='content-button'>Buy on Amazon</button></a>";
+        mainContainer2.appendChild(img2);          
+        mainContainer2.appendChild(div2);
+        mainContainer2.appendChild(button2);
+
+        var mainContainer3 = document.getElementById("myData3");
+        var div3 = document.createElement("h2");
+        var img3 = document.createElement("div");
+        var button3 = document.createElement("div");
+        var Name3 = data[quizanswers][2].displayName;
+        var imageLink3 = data[quizanswers][2].imageLink;
+        var buttonLink3 = data[quizanswers][2].buyLink;
+        div3.innerHTML = Name3; 
+        img3.innerHTML = "<img src='" + imageLink3+ "' class='swaddle' >";
+        button3.innerHTML = "<a href='" +buttonLink3 + "><button type='button' class='content-button'>Buy on Amazon</button></a>";
+        mainContainer3.appendChild(img3);          
+        mainContainer3.appendChild(div3);
+        mainContainer3.appendChild(button3);
+        recommendedAPI (email, quizanswers, childname, Name, buttonLink, Name2, buttonLink2, Name3, buttonLink3);
+
+        var headertext = document.getElementById("heading");
+        headertext.innerHTML = "Recommended Swaddles for " + childname;
+        
+    
+}}
